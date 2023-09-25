3def69cd075421ebbb3111748b18a195e1c8850a
Payton Tung On Yau <tungon@gmail.com>
2023-09-25 22:57:48 +0100

    code update
    code update
    some revised figures
    info for Fig 2B

--- R-scripts/20230921_Fig_2_merged_length_distribution.R
+++ R-scripts/20230921_Fig_2_merged_length_distribution.R
@@ -1,5 +1,7 @@
 
+# data extracted from "rep-seqs_233_226_3_2-with-phyla-no-mitochondria-no-chloroplast.qzv"
 df = read.csv("16s_length_distribution.tsv", header = T, sep = ",")
+
 df$SequenceLength <- apply(df, 1, function(x) nchar(x[['Sequence']]))
 # Sort the data frame by SequenceLength
 df <- df[order(df$SequenceLength), ]
@@ -18,11 +20,11 @@ library(ggplot2)
 # Create a line graph
 p = ggplot(plot_df2, aes(x = SequenceLength, y = AccumulatedPercentage)) +
   labs(x = "Sequence Length", y = "Accumulated Percentage (%)", 
-       title = "Distribution of Accumulated Percentage of Sequence Length Frequency from 0.5% to 99.5%") +
+       title = "Accumulated Sequence Length Frequency (%)") +
   theme_classic() + 
-  geom_line(aes(color = SequenceLength), size = 1.5) +
+  geom_line(aes(color = Frequency), size = 2) +
   theme(
-    text = element_text(size = 16, colour = "black"), 
+    text = element_text(size = 15, colour = "black"), 
     axis.ticks = element_line(colour = "black", size = 1.1),
     axis.line = element_line(colour = 'black', size = 1.1),
     axis.text.x = element_text(colour = "black", angle = 0, hjust = 0.5, size = 13, face = "bold"),
@@ -31,14 +33,18 @@ p = ggplot(plot_df2, aes(x = SequenceLength, y = AccumulatedPercentage)) +
     axis.title.x = element_text(color = "black", size = 20, face = "bold"))
 
 # Add vertical lines and text labels at the 2%, 50%, and 98% points
+
+pdf(file = "Fig2B_merged seq_freq.pdf", width = 10,height = 6)
 p + geom_vline(aes(xintercept = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 2))]), 
                linetype="dashed", size = 1.5, color = "wheat4") +
   geom_text(aes(x = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 2))], y = 2, 
-                label = paste("2% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 2))], ")", sep = "")), vjust = 1, color = "wheat4") +
+                label = paste("2% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 2))], ")", sep = "")), vjust = -6.5, color = "wheat4") +
   geom_vline(aes(xintercept = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 50))]), linetype="dashed", size = 1.5, color = "forestgreen") +
   geom_text(aes(x = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 50))], y = 50, 
-                label = paste("50% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 50))], ")", sep = "")), vjust = -1, color = "forestgreen") +
+                label = paste("50% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 50))], ")", sep = "")), vjust = -2, color = "forestgreen") +
   geom_vline(aes(xintercept = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 98))]), linetype="dashed", size = 1.5, color = "coral4") +
   geom_text(aes(x = plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 98))], y = 98, 
-                label = paste("98% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 98))], ")", sep = "")), vjust = -1, color = "coral4") +
-  scale_x_continuous(breaks = seq(min(plot_df$SequenceLength), max(plot_df$SequenceLength), by = 5))
+                label = paste("98% (", plot_df$SequenceLength[which.min(abs(plot_df$AccumulatedPercentage - 98))], ")", sep = "")), vjust = 1.5, color = "coral4") +
+  scale_x_continuous(breaks = c(400, 405, 410, 415, 420, 425, 430))
+
+dev.off() 
